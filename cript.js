44c4b26ff6f471cb8a9cbd8ca6737c94de24c595
Saimol JImenez <saimoljimenez@gmail.com>
2023-01-07 16:35:53 -0600

    crip y descrip

--- cript.js
+++ cript.js
@@ -8,8 +8,39 @@ for (let x = 0; x < palabracrip.length; x++) {
   }
 } */
 /* console.log(palabracrip); */
-
-function crip() {
+/* function crip() {
   let palabracrip1 = document.getElementById("input_text");
-  let crip = palabracrip1.replace(/e/gm, "enter");
+  let text_crip = palabracrip1;
+  text_crip = text_crip.replace(/a/gm, "im");
+  text_crip = text_crip.replace(/e/gm, "enter");
+  text_crip = text_crip.replace(/i/gm, "imes");
+  text_crip = text_crip.replace(/o/gm, "ober");
+  text_crip = text_crip.replace(/u/gm, "ufat");
+
+  document.getElementById("text_salidas").innerHTML = text_crip;
+  document.getElementById("text_salidas").style.display = "inline-block";
+} */
+function crip() {
+  let palabracrip1 = document.getElementById("texto_input").value;
+  let text_crip = palabracrip1;
+  text_crip = text_crip.replace(/e/gm, "enter");
+  text_crip = text_crip.replace(/o/gm, "ober");
+  text_crip = text_crip.replace(/i/gm, "imes");
+  text_crip = text_crip.replace(/a/gm, "ai");
+  text_crip = text_crip.replace(/u/gm, "ufat");
+
+  document.getElementById("text_salidas").innerHTML = text_crip;
+  document.getElementById("text_salidas").style.display = "inline-block";
+}
+function descrip() {
+  let palabracrip1 = document.getElementById("texto_input").value;
+  let text_crip = palabracrip1;
+  text_crip = text_crip.replace(/enter/gm, "e");
+  text_crip = text_crip.replace(/ober/gm, "o");
+  text_crip = text_crip.replace(/imes/gm, "i");
+  text_crip = text_crip.replace(/ai/gm, "a");
+  text_crip = text_crip.replace(/ufat/gm, "u");
+
+  document.getElementById("text_salidas").innerHTML = text_crip;
+  document.getElementById("text_salidas").style.display = "inline-block";
 }

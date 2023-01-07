375d8d129efd33d586daf6781f70f9f20af947a3
Saimol JImenez <saimoljimenez@gmail.com>
2023-01-07 14:25:58 -0600

    cambios crip

--- cript.js
+++ cript.js
@@ -1,1 +1,13 @@
-document.write("hola mundo");
+let palabraDelUsuario = "hola como estan";
+
+function crip(sentence) {
+  let palabracrip = sentence;
+  for (let x = 0; x < palabracrip.length; x++) {
+    if (palabracrip[x] === "a") {
+      palabracrip[x] = "uta";
+      console.log(palabracrip);
+    }
+  }
+}
+
+crip(palabraDelUsuario);
